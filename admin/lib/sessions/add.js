6dfea240b34378631155f35d4f4f6549c607a70a
ransom <auvergnerw@gmail.com>
2016-03-15 10:45:13 -0700

    admin.sessions.add implementation

--- admin/lib/sessions/add.js
+++ admin/lib/sessions/add.js
@@ -1,8 +1,41 @@
 module.exports = add
 
-function add (state, options) {
-  // iss19 wip
-  // var admin = this
+var _ = require('lodash')
+// var accountsFind = require('../accounts/find')
+var accountsFindAll = require('../accounts/find-all')
+var internals = {}
+internals.request = require('../../../utils/request')
+internals.deserialise = require('../../../utils/deserialise')
+internals.serialise = require('../../../utils/serialise')
 
-  return Promise.resolve({ account: {} })
+function add (state, options) {
+  // TODO: use accountsFind 而不是 accountsFindAll
+  //       after updating accountsFind to match admin README doc
+  // return accountsFind(state, {username: options.username})
+  return accountsFindAll(state)
+    .then(function (response) {
+      if (!options || !options.username) {
+        throw new Error('options.username is required')
+      }
+
+      var account_info = _.filter(
+        response, {username: options.username})[0]
+      if (!account_info) {
+        var not_found_err = new Error('account not found')
+        not_found_err.name = 'NotFoundError'
+        throw not_found_err
+      }
+
+      return internals.request({
+        url: state.url + '/accounts/' + account_info.id + '/sessions',
+        method: 'POST',
+        headers: {
+          authorization: 'Bearer ' + state.account.session.id
+        }
+      })
+    }).then(function (response) {
+      return internals.deserialise(response.body, {
+        include: 'account'
+      })
+    })
 }
